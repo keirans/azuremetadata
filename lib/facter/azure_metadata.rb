9190707ad4b932f87bcfd890a96e91e950e29a4a
Steven Hawkins <steven.hawkins@hiscox.com>
2017-10-31 13:08:21 +0000

    Updated API version and parsed tags as structured facts

--- lib/facter/azure_metadata.rb
+++ lib/facter/azure_metadata.rb
@@ -5,7 +5,7 @@
 # This fact exposes the Azure metadata as a Puppet fact in the az_metadata
 # namespace.
 #
-# As there is no consistent way to contain the fact to azure right now we 
+# As there is no consistent way to contain the fact to azure right now we
 # do it to hyperv for the time being. See the README for more information.
 #
 
@@ -13,11 +13,13 @@ require 'open-uri'
 require 'json'
 
 Facter.add(:az_metadata) do
-    confine :virtual => 'hyperv'
-    setcode do
-      url_metadata="http://169.254.169.254/metadata/instance?api-version=2017-04-02"
-      metadataraw = open(url_metadata,"Metadata"=>"true").read
-      metadata = JSON.parse(metadataraw)
-      metadata
-    end
+  confine virtual: 'hyperv'
+  setcode do
+    url_metadata = 'http://169.254.169.254/metadata/instance?api-version=2017-08-01'
+    metadataraw = open(url_metadata, 'Metadata' => 'true').read
+    metadata = JSON.parse(metadataraw)
+    tags = metadata['compute']['tags'].split(';')
+    metadata['compute']['tags'] = Hash[tags.map { |tag| tag.split(':') }]
+    metadata
+  end
 end
