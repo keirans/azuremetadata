2105c884dc22460af10be4e0f19d636038f2297f
Keiran Sweet <Keiran@gmail.com>
2018-03-21 10:20:52 +0000

    Converting module with PDK, Adding Travis CI configuration and shifting codeblock logic around to be within the confine.

--- lib/facter/azure_metadata.rb
+++ lib/facter/azure_metadata.rb
@@ -13,19 +13,17 @@ require 'open-uri'
 require 'json'
 
 begin
-  url_metadata = 'http://169.254.169.254/metadata/instance?api-version=2017-12-01'
-  metadataraw = open(url_metadata, 'Metadata' => 'true', proxy: false).read
-  metadata = JSON.parse(metadataraw)
-
   Facter.add(:az_metadata) do
     confine :virtual => 'hyperv'
     setcode do
+      url_metadata = 'http://169.254.169.254/metadata/instance?api-version=2017-12-01'
+      metadataraw = open(url_metadata, 'Metadata' => 'true', proxy: false).read
+      metadata = JSON.parse(metadataraw)
       tags = metadata['compute']['tags'].split(';')
       metadata['compute']['tags'] = Hash[tags.map { |tag| tag.split(':', 2) }]
       metadata
     end
   end
-
 rescue
   warn 'Unable to resolve az_metadata - This is not an Azure instance or unable to contact the Azure instance-data web server.'
 end
