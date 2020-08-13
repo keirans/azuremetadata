b30ab3701b3f13087a10a356b64e4500a6c7dba8
Krzysztof Krzyżaniak (eloy) <kkrzyzaniak@egnyte.com>
2020-08-13 12:06:13 +0200

    confine would be enough

--- lib/facter/azure_metadata.rb
+++ lib/facter/azure_metadata.rb
@@ -12,18 +12,14 @@
 require 'open-uri'
 require 'json'
 
-begin
-  Facter.add(:az_metadata) do
-    confine Facter::value(:cloud)['provider'] => 'azure'
-    setcode do
-      url_metadata = 'http://169.254.169.254/metadata/instance?api-version=2020-06-01'
-      metadataraw = open(url_metadata, 'Metadata' => 'true', 'User-Agent' => 'Puppet', proxy: false).read
-      metadata = JSON.parse(metadataraw)
-      tags = metadata['compute']['tags'].split(';')
-      metadata['compute']['tags'] = Hash[tags.map { |tag| tag.split(':', 2) }]
-      metadata
-    end
+Facter.add(:az_metadata) do
+  confine Facter::value(:cloud)['provider'] => 'azure'
+  setcode do
+    url_metadata = 'http://169.254.169.254/metadata/instance?api-version=2020-06-01'
+    metadataraw = open(url_metadata, 'Metadata' => 'true', 'User-Agent' => 'Puppet', proxy: false).read
+    metadata = JSON.parse(metadataraw)
+    tags = metadata['compute']['tags'].split(';')
+    metadata['compute']['tags'] = Hash[tags.map { |tag| tag.split(':', 2) }]
+    metadata
   end
-rescue
-  warn 'Unable to resolve az_metadata - This is not an Azure instance or unable to contact the Azure instance-data web server.'
 end
