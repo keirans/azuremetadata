523e49dc9271deb3f615816cb4dc4a224a3deeb5
Krzysztof Krzyżaniak (eloy) <kkrzyzaniak@egnyte.com>
2020-08-13 12:11:34 +0200

    use ruby block

--- lib/facter/azure_metadata.rb
+++ lib/facter/azure_metadata.rb
@@ -13,7 +13,9 @@ require 'open-uri'
 require 'json'
 
 Facter.add(:az_metadata) do
-  confine Facter::value(:cloud)['provider'] => 'azure'
+  confine :cloud do |cloud|
+    cloud['provider'] == 'azure'
+  end
   setcode do
     url_metadata = 'http://169.254.169.254/metadata/instance?api-version=2020-06-01'
     metadataraw = open(url_metadata, 'Metadata' => 'true', 'User-Agent' => 'Puppet', proxy: false).read
